df6e96c7561841db5debe22502c7b4fed3e54fd1
Aya Jafar <103113441+Aya-Jafar@users.noreply.github.com>
2022-05-08 00:26:54 +0300

    Update Finding-a-Shared-Motif.py

--- Bioinformatics/Rozalind/Finding-a-Shared-Motif.py
+++ Bioinformatics/Rozalind/Finding-a-Shared-Motif.py
@@ -9,5 +9,5 @@ print(sub)
 common = { i:len(i) for i in sub if i in seq2 and i in seq3 }
 print(common)
 
-max_val = max(common, key=common.get)
-print(max_val)
+longest_common = max(common, key=common.get)
+print(longest_common)
